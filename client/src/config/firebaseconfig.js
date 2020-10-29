fdf97c03192051f4a2dbf9bdb9c310004818e80e
WhoAdarshPandya <adarshpandya0@gmail.com>
2020-10-29 12:26:44 +0530

    firebase config

---
+++ client/src/config/firebaseconfig.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+import "firebase/analytics";
+require("dotenv/config");
+
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
